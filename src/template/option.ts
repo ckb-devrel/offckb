849741d7c6bd9b43c87ed556c9511669f63a2f54
RetricSu <inneverland2013@gmail.com>
2024-10-25 15:36:23 +0800

    refactor: rm option.json to fix dev create cmd panic

--- src/template/option.ts
+++ src/template/option.ts
@@ -1,5 +1,3 @@
-import templateOpt from './option.json';
-
 export interface BareTemplateOption {
   name: string;
   value: string;
@@ -8,6 +6,23 @@ export interface BareTemplateOption {
   author: string;
 }
 
+const templates: Array<BareTemplateOption> = [
+  {
+    name: 'Remix-Vite Bare Templates',
+    value: 'remix-vite-template',
+    description: 'A full-stack template with Remix-vite and ckb-script-templates',
+    tag: ['remix', 'vite', 'tailwindcss', 'ckb-script-templates', 'typescript', 'rust'],
+    author: 'retric@cryptape.com',
+  },
+  {
+    name: 'Next.js Bare Templates',
+    value: 'next-js-template',
+    description: 'A full-stack template with Next.js framework and ckb-script-templates',
+    tag: ['next.js', 'tailwindcss', 'ckb-script-templates', 'typescript', 'rust'],
+    author: 'retric@cryptape.com',
+  },
+];
+
 export function loadBareTemplateOpts(): Array<BareTemplateOption> {
-  return templateOpt as Array<BareTemplateOption>;
+  return templates;
 }
