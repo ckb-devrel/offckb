36e987d358d55c0c091a55ba253e2a3033062f61
RetricSu <inneverland2013@gmail.com>
2025-04-11 14:37:20 +0800

    feat(template): add js-script-next-js template

--- src/template/option.ts
+++ src/template/option.ts
@@ -8,14 +8,21 @@ export interface BareTemplateOption {
 
 const templates: Array<BareTemplateOption> = [
   {
-    name: 'Remix-Vite Bare Templates',
+    name: 'JS Script with Next.js fullstack template',
+    value: 'js-script-next-js',
+    description: 'A full-stack template with Next-js and ckb-js-vm script',
+    tag: ['next.js', 'tailwindcss', 'ckb-js-vm', 'typescript'],
+    author: 'retric@cryptape.com',
+  },
+  {
+    name: 'Rust Script with Remix-Vite fullstack template',
     value: 'remix-vite-template',
     description: 'A full-stack template with Remix-vite and ckb-script-templates',
     tag: ['remix', 'vite', 'tailwindcss', 'ckb-script-templates', 'typescript', 'rust'],
     author: 'retric@cryptape.com',
   },
   {
-    name: 'Next.js Bare Templates',
+    name: 'Rust Script with Next.js fullstack template',
     value: 'next-js-template',
     description: 'A full-stack template with Next.js framework and ckb-script-templates',
     tag: ['next.js', 'tailwindcss', 'ckb-script-templates', 'typescript', 'rust'],
